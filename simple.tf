be050274760646618ba2de1bfd5b421183a7538b
seshu <seshueee234@gmail.com>
2023-03-24 13:41:53 +0000

    seshu

--- simple.tf
+++ simple.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-  access_key = "AKIAS3H54TQL6RZBY3CT"
-  secret_key = "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"
   region     = "ap-south-1"
 }
 
 
 resource "aws_instance" "ourfirst" {
-  ami           = "ami-0447a12f28fddb066"
+  ami           = "ami-0f8ca728008ff5af4"
   instance_type = "t2.micro"
   }
